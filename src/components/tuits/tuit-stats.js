e545c1fe10c48609e201a8dc91bb7ab100e7055c
Chengcheng Liu <liu.cheng@northeastern.edu>
2022-03-31 22:50:59 -0400

    likes butten icon

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -15,11 +15,12 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
           <span onClick={() => likeTuit(tuit)}>
               {
                 tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
-                  <i className="fas fa-heart me-1" style={{color: 'red'}}></i>
+                  // <i className="fas fa-heart me-1" style={{color: 'red'}}></i>
+                  <i className="fa-solid fa-thumbs-up" style={{color: 'orange'}}></i>
               }
               {
                 tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
-                  <i className="far fa-heart me-1"></i>
+                  <i className="fa-solid fa-thumbs-down" style={{color: 'blue'}}></i>
               }
             {tuit.stats && tuit.stats.likes}
           </span>
